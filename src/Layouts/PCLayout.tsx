e2bf56ab1fd8f27f2d90dff4c09503434f0c0d9e
Lacheln <psionict@naver.com>
2025-10-03 13:34:15 +0900

    feat: pc레이아웃 이미지 추가
    이미지 클릭시 카페로 이동

--- src/Layouts/PCLayout.tsx
+++ src/Layouts/PCLayout.tsx
@@ -1,6 +1,6 @@
 import Card from "../components/Card";
 import { StreamerSchedule } from "../types/schedule";
-
+import StarDreamLogo from "/STARDREAM_.png";
 interface Day {
     key: "mon" | "tue" | "wed" | "thu" | "fri" | "sat" | "sun";
     label: string;
@@ -21,14 +21,23 @@ const PCLayout = ({ days, streamers, weeklyDate }: PCLayoutProps) => {
     return (
         <div className="min-h-screen bg-[#feffef] p-8 ">
             <div className="max-w-[1800px] mx-auto">
-                <h1 className="text-4xl font-bold text-center mb-8">
-                    스타드림 {weeklyDate} 통합시간표
-                </h1>
+                <div className="flex justify-center text-center items-center gap-3 mb-8">
+                    <a href="https://cafe.naver.com/stardreamco">
+                        <img
+                            src={StarDreamLogo}
+                            alt="스타드림로고"
+                            className="w-[30px] h-[30px] xl:w-[36px] xl:h-[36px]"
+                        />
+                    </a>
+                    <div className="text-3xl xl:text-4xl font-bold text-center">
+                        스타드림 {weeklyDate} 통합시간표
+                    </div>
+                </div>
 
                 <div className="grid grid-cols-8 gap-4 mb-4">
                     <div className="w-32"></div>
                     {days.map((day) => (
-                        <div key={day.key} className="text-center font-bold text-2xl">
+                        <div key={day.key} className="text-center font-bold md:text-xl lg:text-2xl">
                             {day.label}
                         </div>
                     ))}
@@ -37,7 +46,9 @@ const PCLayout = ({ days, streamers, weeklyDate }: PCLayoutProps) => {
                 {streamers.map((streamer) => (
                     <div key={streamer.name} className="grid grid-cols-8 gap-4 mb-4">
                         <div className="flex items-center justify-center">
-                            <div className="font-bold text-xl">{streamer.name}</div>
+                            <div className="font-bold  md:text-xl  lg:text-2xl">
+                                {streamer.name}
+                            </div>
                         </div>
 
                         {days.map((day) => (
