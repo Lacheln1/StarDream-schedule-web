3e994311e8360fb78923687490afffc2f5172488
lacheln <psionict@naver.com>
2025-10-07 00:03:50 +0900

    fix: vercel 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import MainPage from "./pages/MainPage";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 const App = () => {
     return (
         <div>
